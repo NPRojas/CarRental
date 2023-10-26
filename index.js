d2342a23751a4d92af8e5c5a56e553299deeaf59
NPRojas <63315208+NPRojas@users.noreply.github.com>
2023-10-26 19:43:52 -0400

    write html for form

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+windows.onload = init;
+
+function init() {
+    const btn = document.querySelector('button');
+    btn.onclick = calculateCarRental;
+}
+
+function calculateCarRental() {
+    //Get the inputs
+
+    //convert inputs to numbers/parse floats?
+
+    //Calculate the unknown values
+
+    //Display the output 
+}
