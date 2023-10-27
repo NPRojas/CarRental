e93772c7dc26f485df19d35a8ea85f92272583b0
NPRojas <63315208+NPRojas@users.noreply.github.com>
2023-10-26 22:00:37 -0400

    add js functionality to calculator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-windows.onload = init;
+window.onload = init;
 
 function init() {
     const btn = document.querySelector('button');
@@ -8,11 +8,40 @@ function init() {
 }
 
 function calculateCarRental() {
-    //Get the inputs
+    const dailyCarRental = 29.99;
 
-    //convert inputs to numbers/parse floats?
+    //Get the inputs
+    const daysRented = document.querySelector('#numberofDaysRented').value;
 
     //Calculate the unknown values
+    const carRentalTotal = dailyCarRental * daysRented;
+    let options = 0;
+    let surcharge = 0;
+    
+    if(document.querySelector('#tollTag').checked){
+        options += 3.95 * daysRented;
+    }
+    if(document.querySelector('#gps').checked){
+        options += 2.95 * daysRented;
+    }
+    if(document.querySelector('#roadsideAssistance').checked){
+        options += 2.95 * daysRented;
+    }
+
+    if(document.querySelector('#yes').checked){
+        surcharge = 0.3 * dailyCarRental;
+    }
+
+    const totalDue = carRentalTotal + options + surcharge;
 
     //Display the output 
-}
+    const carRentalOutput = document.querySelector('#carRental');
+    const optionsOutput = document.querySelector('#options');
+    const under25Output = document.querySelector('#under25Surcharge');
+    const totalDueOutput = document.querySelector('#totalDue');
+
+    carRentalOutput.textContent += `${carRentalTotal.toFixed(2)}`;
+    optionsOutput.textContent += `${options.toFixed(2)}`;
+    under25Output.textContent += `${surcharge.toFixed(3)}`;
+    totalDueOutput.textContent += `${totalDue.toFixed(2)}`;
+}   
